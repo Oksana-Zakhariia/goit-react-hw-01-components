e40257913468c635c2449f4dd12b5f65412ab269
Oksana-Zakhariia <okcahka.l@gmail.com>
2023-01-02 20:19:33 +0300

    friends

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,6 +1,7 @@
 import { PropTypes } from  'prop-types'
 import { FriendListItem } from "components/FriendListItem/FriendListItem";
 import friends from 'components/friends.json'
+console.log(friends);
 export const FriendList = ({friends}) => {
     return <ul>        
         {friends.map(friend => {
