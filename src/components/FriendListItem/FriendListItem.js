89c77e011687ebd03f218d38c4fd86b5a53238df
Oksana-Zakhariia <okcahka.l@gmail.com>
2023-01-02 20:38:48 +0300

    ass

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -9,11 +9,7 @@ export const FriendListItem = ( props) => {
 </>
 }
 
-{/* <li class="item">
-  <span class="status"></span>
-  <img class="avatar" src="" alt="User avatar" width="48" />
-  <p class="name"></p>
-</li> */}
+
 FriendListItem.propTypes = {
     isOnline: PropTypes.bool.isRequired,
     avatar: PropTypes.string.isRequired,
