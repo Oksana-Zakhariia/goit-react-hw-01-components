4ae68a5643791400c7729486d11c8cf969680328
Oksana-Zakhariia <okcahka.l@gmail.com>
2023-01-02 22:36:29 +0300

    kj

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 export const TransactionHistory = ({ items }) => {
 
     return (<table className="transaction-history">
@@ -20,3 +21,9 @@ export const TransactionHistory = ({ items }) => {
         </tbody>
     </table>);
 }
+TransactionHistory.propTypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        amount: PropTypes.string.isRequired,
+            currency: PropTypes.string.isRequired,
+        }))   }
